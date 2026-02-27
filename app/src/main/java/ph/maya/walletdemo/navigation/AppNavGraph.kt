8455d9efcf68b1982d1df64638251c216af3d99b
jom <>
2026-02-27 11:18:51 +0800

    fix(transaction-ui): fix redirection issue after logout

--- app/src/main/java/ph/maya/walletdemo/navigation/AppNavGraph.kt
+++ app/src/main/java/ph/maya/walletdemo/navigation/AppNavGraph.kt
@@ -55,7 +55,7 @@ fun AppNavGraph(
             TransactionsRoute(
                 viewModel = hiltViewModel(it),
                 onLoggedOut = {
-                    navController.navigate(Destinations.TRANSACTIONS) {
+                    navController.navigate(Destinations.LOGIN) {
                         popUpTo(0) { inclusive = true }
                     }
                 }
