9872a6a11013b8b11f9042e5eff50263eda435a1
jom <>
2026-02-27 11:25:27 +0800

    feat(domain): add Transaction model

---
+++ app/src/main/java/ph/maya/walletdemo/domain/model/Transaction.kt
@@ -0,0 +1,9 @@
+package ph.maya.walletdemo.domain.model
+
+import java.util.UUID
+
+data class Transaction(
+    val id: String = UUID.randomUUID().toString(),
+    val amount: Double,
+    val currency: String = "PHP"
+)
