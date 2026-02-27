271a3b7681cb82a6a5550e772145ef42f72778e8
jom <>
2026-02-27 09:23:11 +0800

    feat(nav): add destinations routes

---
+++ app/src/main/java/ph/maya/walletdemo/navigation/Destinations.kt
@@ -0,0 +1,8 @@
+package ph.maya.walletdemo.navigation
+
+object Destinations {
+    const val LOGIN = "login"
+    const val WALLET = "wallet"
+    const val SEND_MONEY = "send_money"
+    const val TRANSACTIONS = "transactions"
+}
