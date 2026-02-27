83f3d974e95c85d736248b656851464a902b9b3e
jom <>
2026-02-27 09:53:56 +0800

    feat(auth): migrate LoginViewModel to HiltViewModel

--- app/src/main/java/ph/maya/walletdemo/presentation/auth/LoginViewModel.kt
+++ app/src/main/java/ph/maya/walletdemo/presentation/auth/LoginViewModel.kt
@@ -2,13 +2,16 @@ package ph.maya.walletdemo.presentation.auth
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import ph.maya.walletdemo.domain.usecase.auth.LoginUseCase
+import javax.inject.Inject
 
-class LoginViewModel(
+@HiltViewModel
+class LoginViewModel  @Inject constructor(
     private val loginUseCase: LoginUseCase
 ) : ViewModel() {
 
